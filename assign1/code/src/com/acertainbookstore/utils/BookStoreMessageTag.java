75f48875e73047866df9d88c19ab4010a4a75142
Rasmus Wriedt Larsen <rasmuswriedtlarsen@gmail.com>
2015-11-20 21:45:11 +0100

    formatting of BookStoreMessageTag

--- assign1/code/src/com/acertainbookstore/utils/BookStoreMessageTag.java
+++ assign1/code/src/com/acertainbookstore/utils/BookStoreMessageTag.java
@@ -1,12 +1,13 @@
 /**
- * 
+ *
  */
 package com.acertainbookstore.utils;
 
 /**
  * BookStoreMessageTag implements the messages supported in the bookstore
- * 
+ *
  */
 public enum BookStoreMessageTag {
-	ADDBOOKS, LISTBOOKS, ADDCOPIES, GETBOOKS, BUYBOOKS, UPDATEEDITORPICKS, EDITORPICKS, REMOVEALLBOOKS, REMOVEBOOKS, GETSTOCKBOOKSBYISBN;
+    ADDBOOKS, LISTBOOKS, ADDCOPIES, GETBOOKS, BUYBOOKS, UPDATEEDITORPICKS,
+    EDITORPICKS, REMOVEALLBOOKS, REMOVEBOOKS, GETSTOCKBOOKSBYISBN;
 }
