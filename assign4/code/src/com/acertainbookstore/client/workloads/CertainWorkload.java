95a43b625528b8fd4e85d1c6e3a700e419f89512
Truls Asheim <truls@asheim.dk>
2015-12-15 17:30:07 +0100

    Removed redundant import

--- assign4/code/src/com/acertainbookstore/client/workloads/CertainWorkload.java
+++ assign4/code/src/com/acertainbookstore/client/workloads/CertainWorkload.java
@@ -5,7 +5,6 @@ package com.acertainbookstore.client.workloads;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
